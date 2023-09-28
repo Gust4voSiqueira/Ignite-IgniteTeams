db0898a650ec231c019653ee908e7f31edd774d1
gustavo.siqueira <gustavo.siqueira@cwi.com.br>
2023-09-28 10:45:19 -0300

    Adicionando funcionalidade de criar grupos

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,12 +1,31 @@
+import { useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
+import { AppError } from '@utils/AppError'
 import { Container, Content, Icon } from './styles'
 import { Button, Input, Header, Highlight } from '@components/index'
+import { groupCreate } from '@storage/group/groupCreate'
+import { Alert } from 'react-native'
 
 export function NewGroup() {
+  const [group, setGroup] = useState('')
   const navigation = useNavigation()
 
-  function handleNew() {
-    navigation.navigate('players', { group: 'Rocket' })
+  async function handleNew() {
+    try {
+      if (group.trim().length === 0) {
+        return Alert.alert('Novo Grupo', 'Informe o nome da turma.')
+      }
+
+      await groupCreate(group)
+      navigation.navigate('players', { group })
+      setGroup('')
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert('Novo Grupo', error.message)
+      } else {
+        Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo.')
+      }
+    }
   }
 
   return (
@@ -21,7 +40,11 @@ export function NewGroup() {
           subtitle="crie a turma para adicionar pessoas."
         />
 
-        <Input placeholder="Nome da turma" />
+        <Input
+          placeholder="Nome da turma"
+          onChangeText={setGroup}
+          value={group}
+        />
 
         <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
